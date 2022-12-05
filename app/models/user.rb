8800b6fea7217d7893dad2ae77c1d1e2a50660eb
hminami373 <south.s.373.n.y@gmail.com>
2022-12-05 16:54:01 +0900

    モデル作成・新規登録画面編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  validates :nickname, length: { maximum: 40 }
+  
+  with_options presence: true do
+    validates :nickname, :user_birth_date, :password_confirmation
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+  end
+
+  validates :encrypted_password, :password_confirmation,
+  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "は半角英数で入力して下さい" }
+
 end
