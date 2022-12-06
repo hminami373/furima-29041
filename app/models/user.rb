97ea9e587d5d508cdd9e85b8f147db05f712d193
hminami373 <south.s.373.n.y@gmail.com>
2022-12-06 19:13:12 +0900

    Active Storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname, length: { maximum: 40 }
 
   with_options presence: true do
