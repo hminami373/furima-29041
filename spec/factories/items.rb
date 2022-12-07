6142b6e55d3341720ebc1ae68f32878c8d85858d
hminami373 <south.s.373.n.y@gmail.com>
2022-12-07 22:41:23 +0900

    単体テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    item_name                  { 'オムライス' }
+    item_info                  { '山田着用品' }
+    item_category_id              { 2 }
+    item_sales_status_id          { 2 }
+    item_scheduled_delivery_id    { 2 }
+    prefecture_id                 { 2 }
+    item_shipping_fee_status_id   { 2 }
+    item_price                    {1000}
+    association :user 
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/food_omurice.png'), filename: 'food_omurice.png')
+    end
+
   end
 end
