7c06f7b8c150e5e6c850d256f43506934b32ca33
hminami373 <south.s.373.n.y@gmail.com>
2022-12-09 21:31:40 +0900

    プルリクエスト前修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-
     if @item.save
       redirect_to root_path
     else
@@ -25,11 +24,14 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    return if current_user == @item.user
+
+    redirect_to root_path
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
