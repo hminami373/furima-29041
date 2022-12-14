4092b225a790625deeb057ae57aed7dc91c85527
hminami373 <south.s.373.n.y@gmail.com>
2022-12-14 23:10:24 +0900

    STEP2 PAY.JPを利用してクレジットカード決済ができるようにする

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,10 +1,10 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :city, :prefecture_id, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :city, :prefecture_id, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id, :item_id
-    validates :city, :address
+    validates :city, :address, :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid."}
   end
--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
     address     {'１−１'}
     building      {'第一ビル'} 
     phone_number  {'09012345678'} 
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
