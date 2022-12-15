87765850087409626ba10c573d9ff250a8672fec
hminami373 <south.s.373.n.y@gmail.com>
2022-12-15 16:02:50 +0900

    修正1回目追記

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,11 +4,11 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id, :item_id
-    validates :prefecture_id, :city, :address, :token
+    validates :city, :address, :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     # 購入情報を保存し、変数orderに代入する
