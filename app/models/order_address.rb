e5d96d16e369a3036ec9e3bd1dec32e9ff964040
hminami373 <south.s.373.n.y@gmail.com>
2022-12-15 15:51:06 +0900

    挙動確認依頼後修正1回目

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,7 +4,7 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id, :item_id
-    validates :city, :address, :token
+    validates :prefecture_id, :city, :address, :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
   end
