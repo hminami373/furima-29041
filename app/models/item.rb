dd6b22f3d3e4f3d4adbb738a809adc2aca0bcf83
hminami373 <south.s.373.n.y@gmail.com>
2023-02-14 20:45:27 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :order
-  # has_one :order
+
   belongs_to :item_category
   belongs_to :item_sales_status
   belongs_to :item_shipping_fee_status
